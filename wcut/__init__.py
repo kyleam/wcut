8f177ea09a6e8749bdbea4521434d0f2e2dff2cd
Kyle Meyer <meyerkya@gmail.com>
2013-05-03 00:43:17 -0400

    extract out most of the script to core module

--- wcut/__init__.py
+++ wcut/__init__.py
@@ -1,4 +1,34 @@
-import os
 from . import _version
+from ._core import get_lines, process_lines, write_fields
 
 __fullversion__, __version__ = _version.get_version()
+
+_script_doc = """\
+Select fields by header keywords
+
+Usage: wcut [options] WORDS [FILE]...
+
+Arguments:
+  WORDS
+      comma-separated keyword list
+  FILE
+      read from stdin if not present or -
+
+Options:
+  -h, --help
+      print help message
+  -l, --line=N
+      line N (1-based) of file to match WORDS against.
+      Any lines before this point are discarded.
+      [default: 1]
+  -d, --delimiter=DELIM
+      [default: " "]
+  -i, --ignore-case
+  -o OUTFILE
+
+Example:
+  $ echo "cat hat bat\\n1 2 3\\n4 5 6" | wcut hat,cat
+  hat cat
+  2 1
+  5 4
+"""
---
+++ wcut/_core.py
@@ -0,0 +1,35 @@
+import sys
+import fileinput
+from itertools import product
+
+
+def match_fields(fields, matches, ignore_case=False, **kwargs):
+    already_yielded = set()
+    if ignore_case:
+        fields = [f.lower() for f in fields]
+        matches = [m.lower() for m in matches]
+    fields = [(i, field) for i, field in enumerate(fields)]
+    for match, field in product(matches, fields):
+        if match in field[1] and field[0] not in already_yielded:
+            yield field[0]
+            already_yielded.add(field[0])
+
+
+def get_lines(files):
+    for line in fileinput.input(files):
+        yield fileinput.lineno(), line
+
+
+def process_lines(lines, match_lineno, words, delim, **kwargs):
+    for lineno, line in lines:
+        fields = line.strip().split(delim)
+        if lineno < match_lineno:
+            continue
+        elif lineno == match_lineno:
+            keep_fields = [f for f in match_fields(fields, words, **kwargs)]
+        yield [fields[i] for i in keep_fields]
+
+
+def write_fields(ofh, field_lines, delim):
+    for fields in field_lines:
+        ofh.write(delim.join(fields) + '\n')

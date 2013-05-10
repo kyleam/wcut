1166262d496ff1eeccc54d074a8b6c100b015eb8
Kyle Meyer <meyerkya@gmail.com>
2013-05-10 01:57:18 -0400

    unittest to py.test

--- test_wcut.py
+++ test_wcut.py
@@ -1,140 +1,160 @@
-import unittest
+import pytest
 from io import StringIO
 
 from wcut import match_fields, process_lines, write_fields
 
 
-class TestCore(unittest.TestCase):
+@pytest.fixture
+def animals():
+    return ['seal', 'otter']
 
-    def test_no_matching_fields(self):
-        fields = ['f1', 'f2']
-        searches = ['seal', 'parrot']
-        self.assertEqual(list(match_fields(fields, searches)), [])
+@pytest.fixture
+def digits():
+    return ['pinky', 'big']
 
-    def test_match_with_empty_field(self):
-        fields = ['f1', 'f2']
-        searches = ['']
-        self.assertEqual(list(match_fields(fields, searches)), [])
 
-    def test_match_case_sensitive(self):
-        fields = ['Seal', 'PARROT']
-        searches = ['seal', 'parrot']
-        self.assertEqual(list(match_fields(fields, searches)), [])
-        self.assertEqual(list(match_fields(fields, searches, ignore_case=True)),
-                         [0, 1])
+@pytest.fixture(params=[' ', ',', '\t'])
+def delim(request):
+    return request.param
 
-    def test_match_partial(self):
-        fields = ['seal', 'parrot']
-        searches = ['se', 'par']
-        self.assertEqual(list(match_fields(fields, searches)), [0, 1])
 
-    def test_match_wholename(self):
-        fields = ['seal', 'parrot']
-        searches = ['se', 'par']
-        self.assertEqual(list(match_fields(fields, searches, wholename=True)),
-                         [])
-        self.assertEqual(list(match_fields(fields, fields, wholename=True)),
-                         [0, 1])
+## field matching
 
-    def test_match_with_repeated_fields(self):
-        fields = ['seal', 'parrot', 'parrot']
-        searches = ['seal', 'parrot']
-        self.assertEqual(list(match_fields(fields, searches)), [0, 1, 2])
 
-    def test_match_with_repeated_searches(self):
-        fields = ['seal', 'parrot']
-        searches = ['seal', 'parrot', 'parrot']
-        self.assertEqual(list(match_fields(fields, searches)), [0, 1])
+def test_no_matching_fields(digits, animals):
+    assert list(match_fields(digits, animals)) == []
 
-    def test_singlespace_delim(self):
-        lines = [(1, 'c1 c2 c3'),
-                 (2, '1 2 3')]
-        expected_result = [['c1', 'c2', 'c3'],
-                           ['1', '2', '3']]
-        result = list(process_lines(lines, ' ', ['c1', 'c2', 'c3']))
-        self.assertEqual(result, expected_result)
 
-    def test_tab_delim(self):
-        lines = [(1, 'c1\tc2\tc3'),
-                 (2, '1\t2\t3')]
-        expected_result = [['c1', 'c2', 'c3'],
-                           ['1', '2', '3']]
+def test_match_with_empty_field(digits):
+    assert list(match_fields(digits, [''])) == []
 
-        result = list(process_lines(lines, '\t', ['c1', 'c2', 'c3']))
-        self.assertEqual(result, expected_result)
 
-    def test_match_second_line_match(self):
-        lines = [(1, 'not considered'),
-                 (2, 'matched nomatch'),
-                 (3, '1 2')]
-        expected_result = [['matched',],
-                           ['1',]]
+def test_match_case_sensitive(animals):
+    upper = [a.upper() for a in animals]
+    assert list(match_fields(upper, animals)) == []
+    assert list(match_fields(upper, animals, ignore_case=True)) == [0, 1]
 
-        result = list(process_lines(lines, ' ', ['matched',],
-                                    match_lineno=2))
-        self.assertEqual(result, expected_result)
 
-    def test_match_in_reverse_order(self):
-        lines = [(1, 'c1 c2 c3'),
-                 (2, '1 2 3')]
-        expected_result = [['c2', 'c1'],
-                           ['2', '1']]
+def test_match_partial(animals):
+    partial = [a[:2] for a in animals]
+    assert list(match_fields(animals, partial)) == [0, 1]
 
-        result = list(process_lines(lines, ' ', ['c2', 'c1']))
-        self.assertEqual(result, expected_result)
 
-    def test_lines_ends_with_blank_line(self):
-        lines = [(1, 'c1 c2 c3'),
-                 (2, '\n')]
-        expected_result = [['c1', 'c2'],
-                           '\n']
+def test_match_wholename(animals):
+    partial = [a[:2] for a in animals]
+    assert list(match_fields(animals, partial, wholename=True)) == []
+    assert list(match_fields(animals, animals, wholename=True)) == [0, 1]
 
-        result = list(process_lines(lines, ' ', ['c1', 'c2']))
-        self.assertEqual(result, expected_result)
 
-    def test_lines_with_blank_line_inside(self):
-        lines = [(1, 'c1 c2 c3'),
-                 (2, '\n'),
-                 (3, '1 2 3')]
-        expected_result = [['c1', 'c2'],
-                           '\n',
-                           ['1', '2']]
+def test_match_with_repeated_fields(animals):
+    repeat = animals + animals[-1:]
+    assert list(match_fields(repeat, animals)) == [0, 1, 2]
 
-        result = list(process_lines(lines, ' ', ['c1', 'c2']))
-        self.assertEqual(result, expected_result)
 
-    def test_multisource_lines(self):
-        lines = [(1, 'c1 c2 c3'),
-                 (2, '1 2 3'),
-                 (1, 'c1 c2 c3'),
-                 (2, '4 5 6')]
-        expected_result = [['c1', 'c2', 'c3'],
-                           ['1', '2', '3'],
-                           ['4', '5', '6']]
+def test_match_with_repeated_searches(animals):
+    repeat = animals + ['parrot']
+    assert list(match_fields(animals, repeat)) == [0, 1]
 
-        result = list(process_lines(lines, ' ', ['c1', 'c2', 'c3']))
-        self.assertEqual(result, expected_result)
 
-    def test_multisource_lines_different_order(self):
-        lines = [(1, 'c1 c2 c3'),
-                 (2, '1 2 3'),
-                 (1, 'c1 c3 c2'),
-                 (2, '4 6 5')]
-        expected_result = [['c1', 'c2', 'c3'],
-                           ['1', '2', '3'],
-                           ['4', '5', '6']]
+## delim
 
-        result = list(process_lines(lines, ' ', ['c1', 'c2', 'c3']))
-        self.assertEqual(result, expected_result)
 
-    def test_write_fields(self):
-        ofh = StringIO()
-        towrite = [['c1', 'c2'],
-                   ['1', '2']]
-        write_fields(ofh, towrite, ' ')
-        result = ofh.getvalue()
-        expected_result = 'c1 c2\n1 2\n'
-        self.assertEqual(result, expected_result)
+def test_delims(delim):
+    expected = [['c1', 'c2', 'c3'], ['1', '2', '3']]
+    lines = [(lineno, delim.join(line)) for lineno, line in enumerate(expected, 1)]
+    result = list(process_lines(lines, delim, ['c1', 'c2', 'c3']))
+    assert result == expected
 
-if __name__ == '__main__':
-    unittest.main()
+
+## line skip
+
+
+def test_match_second_line_match():
+    lines = [(1, 'not considered'),
+             (2, 'matched nomatch'),
+             (3, '1 2')]
+    expected_result = [['matched',],
+                       ['1',]]
+
+    result = list(process_lines(lines, ' ', ['matched',],
+                                match_lineno=2))
+    assert result == expected_result
+
+
+## order change
+
+
+def test_match_in_reverse_order():
+    lines = [(1, 'c1 c2 c3'),
+             (2, '1 2 3')]
+    expected_result = [['c2', 'c1'],
+                       ['2', '1']]
+
+    result = list(process_lines(lines, ' ', ['c2', 'c1']))
+    assert result == expected_result
+
+
+## blank lines
+
+
+def test_lines_ends_with_blank_line():
+    lines = [(1, 'c1 c2 c3'),
+             (2, '\n')]
+    expected_result = [['c1', 'c2'],
+                       '\n']
+
+    result = list(process_lines(lines, ' ', ['c1', 'c2']))
+    assert result == expected_result
+
+
+def test_lines_with_blank_line_inside():
+    lines = [(1, 'c1 c2 c3'),
+             (2, '\n'),
+             (3, '1 2 3')]
+    expected_result = [['c1', 'c2'],
+                       '\n',
+                       ['1', '2']]
+
+    result = list(process_lines(lines, ' ', ['c1', 'c2']))
+    assert result == expected_result
+
+
+## multiple inputs
+
+
+def test_multisource_lines():
+    lines = [(1, 'c1 c2 c3'),
+             (2, '1 2 3'),
+             (1, 'c1 c2 c3'),
+             (2, '4 5 6')]
+    expected_result = [['c1', 'c2', 'c3'],
+                       ['1', '2', '3'],
+                       ['4', '5', '6']]
+    result = list(process_lines(lines, ' ', ['c1', 'c2', 'c3']))
+    assert result == expected_result
+
+
+def test_multisource_lines_different_order():
+    lines = [(1, 'c1 c2 c3'),
+             (2, '1 2 3'),
+             (1, 'c1 c3 c2'),
+             (2, '4 6 5')]
+    expected_result = [['c1', 'c2', 'c3'],
+                       ['1', '2', '3'],
+                       ['4', '5', '6']]
+
+    result = list(process_lines(lines, ' ', ['c1', 'c2', 'c3']))
+    assert result == expected_result
+
+
+## write fields
+
+
+def test_write_fields():
+    ofh = StringIO()
+    towrite = [['c1', 'c2'],
+               ['1', '2']]
+    write_fields(ofh, towrite, ' ')
+    result = ofh.getvalue()
+    expected_result = 'c1 c2\n1 2\n'
+    assert result == expected_result

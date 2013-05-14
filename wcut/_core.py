87ed36b9367f7db949b1abf896a3f5610a455d64
Kyle Meyer <meyerkya@gmail.com>
2013-05-14 15:48:27 -0400

    reorder functions in consistent way

--- wcut/_core.py
+++ wcut/_core.py
@@ -1,6 +1,43 @@
 from itertools import product
 
 
+def extract_fields(lines, delim, searches, match_lineno=1, **kwargs):
+    """Return generator of fields matching `searches`
+
+    Parameters
+    ----------
+    lines : iterable
+        returns line number (1-based) and line (str)
+    delim : str
+        delimiter to split line by to produce fields
+    searches : iterable
+        returns search (str) to match against line fields
+    match_lineno : int
+        line number of line to split and search fields
+
+    kwargs passed to `match_fields`
+    """
+    processed_matchline = False
+    keep_idx = []
+    for lineno, line in lines:
+        if lineno < match_lineno or delim not in line:
+            yield [line]
+            continue
+
+        fields = line.split(delim)
+        if lineno == match_lineno:
+            keep_idx = list(match_fields(fields, searches, **kwargs))
+            if processed_matchline:
+                ## already yielded header once, so don't repeat it for
+                ## additional files
+                continue
+            processed_matchline = True
+        keep_fields = [fields[i] for i in keep_idx]
+
+        if keep_fields:
+            yield keep_fields
+
+
 def match_fields(fields, searches,
                  ignore_case=False, wholename=False, complement=False):
     """Return fields that match searches
@@ -39,40 +76,3 @@ def _complete_match(search, target):
 
 def _partial_match(search, target):
     return search in target
-
-
-def extract_fields(lines, delim, searches, match_lineno=1, **kwargs):
-    """Return generator of fields matching `searches`
-
-    Parameters
-    ----------
-    lines : iterable
-        returns line number (1-based) and line (str)
-    delim : str
-        delimiter to split line by to produce fields
-    searches : iterable
-        returns search (str) to match against line fields
-    match_lineno : int
-        line number of line to split and search fields
-
-    kwargs passed to `match_fields`
-    """
-    processed_matchline = False
-    keep_idx = []
-    for lineno, line in lines:
-        if lineno < match_lineno or delim not in line:
-            yield [line]
-            continue
-
-        fields = line.split(delim)
-        if lineno == match_lineno:
-            keep_idx = list(match_fields(fields, searches, **kwargs))
-            if processed_matchline:
-                ## already yielded header once, so don't repeat it for
-                ## additional files
-                continue
-            processed_matchline = True
-        keep_fields = [fields[i] for i in keep_idx]
-
-        if keep_fields:
-            yield keep_fields

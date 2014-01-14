bda59fc1fd4a870b00a3d2988a8f9dc69388ba03
Kyle Meyer <kyle@kyleam.com>
2014-01-14 00:43:20 -0500

    Use printf in tests instead of echo

--- tests/functional/run-tests.sh
+++ tests/functional/run-tests.sh
@@ -8,42 +8,42 @@ testdir=test-results
 mkdir -p $testdir
 testfile=$testdir/test-output
 
-echo '* DY - last name column' > $testfile
+printf '* DY - last name column\n' > $testfile
 wcut last death-years.txt >> $testfile
-echo '* DY - last name column, complement' >> $testfile
+printf '* DY - last name column, complement\n' >> $testfile
 wcut last -v death-years.txt >> $testfile
-echo '* DY - last name column, only delimiter' >> $testfile
+printf '* DY - last name column, only delimiter\n' >> $testfile
 wcut last -s death-years.txt >> $testfile
-echo '* DY - last name column, wholename, no match' >> $testfile
+printf '* DY - last name column, wholename, no match\n' >> $testfile
 wcut last -w death-years.txt >> $testfile
-echo '* DY - last name column, wholename' >> $testfile
+printf '* DY - last name column, wholename\n' >> $testfile
 wcut last_name -w death-years.txt >> $testfile
-echo '* DY - year and first name column, reverse order' >> $testfile
+printf '* DY - year and first name column, reverse order\n' >> $testfile
 wcut year,first death-years.txt >> $testfile
 
-echo '* WM - config column' >> $testfile
+printf '* WM - config column\n' >> $testfile
 wcut -d',' -l2 Config tiling-wms.csv >> $testfile
-echo '* WM - config column, wrong match line' >> $testfile
+printf '* WM - config column, wrong match line\n' >> $testfile
 wcut -d',' -l1 Config tiling-wms.csv &>> $testfile
-echo '* WM - config column, no preheader' >> $testfile
+printf '* WM - config column, no preheader\n' >> $testfile
 wcut -d',' -l2 -r Config tiling-wms.csv >> $testfile
-echo '* WM - config column, complement' >> $testfile
+printf '* WM - config column, complement\n' >> $testfile
 wcut -d',' -l2 -v Config tiling-wms.csv >> $testfile
-echo '* WM - config column, only delimiter' >> $testfile
+printf '* WM - config column, only delimiter\n' >> $testfile
 wcut -d',' -l2 -s Config tiling-wms.csv >> $testfile
-echo '* WM - config column, ignore case' >> $testfile
+printf '* WM - config column, ignore case\n' >> $testfile
 wcut -d',' -l2 -i config tiling-wms.csv >> $testfile
-echo '* WM - config column, wholename, no match' >> $testfile
+printf '* WM - config column, wholename, no match\n' >> $testfile
 wcut -d',' -l2 -w Config tiling-wms.csv >> $testfile
-echo '* WM - config column, wholename' >> $testfile
+printf '* WM - config column, wholename\n' >> $testfile
 wcut -d',' -l2 -w "Configured with" tiling-wms.csv >> $testfile
-echo '* WM - written in and window manager, reverse order' >> $testfile
+printf '* WM - written in and window manager, reverse order\n' >> $testfile
 wcut -d',' -l2 Writ,Man tiling-wms.csv >> $testfile
-echo '* WM - written in and window manager, reverse order, ignore case' >> $testfile
+printf '* WM - written in and window manager, reverse order, ignore case\n' >> $testfile
 wcut -d',' -l2 -i writ,man tiling-wms.csv >> $testfile
 
-echo '* STDIN - first col' >> $testfile
-echo -e "first second third\n1 2 3\n4 5 6" | wcut first - >> $testfile
+printf '* STDIN - first col\n' >> $testfile
+printf "first second third\n1 2 3\n4 5 6" | wcut first - >> $testfile
 
 ## Outfile test
 wcut last death-years.txt -o $testdir/dy-lastname-outfile
